28a90cb46f6db8813827f511efe3dc1d9a651eee
Artem <artemm3note@gmail.com>
2023-08-17 19:54:22 +0300

    Add styles for home page
    Don't forget to fix bud with page height.

---
+++ src/scripts/homePage.js
@@ -0,0 +1,14 @@
+const homePage = 
+`
+<div class="page" id="homePage">
+    <div class="main__home-info">
+        <p>Restaurant</p>
+        <svg xmlns="http://www.w3.org/2000/svg" height="60px" viewBox="0 0 384 512"><style>svg{fill:#ffffff}</style><path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg>
+    </div>
+    <p class="main__home-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas quae maiores et sed blanditiis iusto repellat maxime. Asperiores voluptatem, pariatur enim suscipit distinctio esse repellat ad assumenda ipsum, eius eum!
+    Magni voluptate nisi modi culpa quia molestias voluptatem ex excepturi sed at sint amet, adipisci voluptatibus mollitia animi ipsum eius soluta? Accusamus in sit temporibus mollitia libero minima quasi omnis.
+    Doloribus ipsa, voluptates minima et natus deserunt architecto quisquam esse sint praesentium? Soluta mollitia omnis ut eveniet, magnam minima temporibus enim, libero vero sapiente quos! Architecto ratione officiis aut dolore.
+    Itaque earum non tenetur, fugit, cupiditate repudiandae quasi incidunt ipsam consectetur voluptates esse iste quisquam aliquid nam nemo doloremque modi, voluptate accusamus quis dicta molestias. Natus molestias nam aperiam cumque!
+    Deserunt nam dolorem labore ut aut maiores itaque voluptate fuga. Labore tempore, quidem dicta ea quas voluptate incidunt quo animi non recusandae suscipit a neque eaque sint, minus, veniam quae?</p>
+</div>
+`
---
+++ src/scripts/menuPage.js
@@ -0,0 +1,92 @@
+const menuPage =
+`
+<div class="page" id="menuPage">
+    <div class="menu__dish">
+        <img src="assets/imgs/dish.jpg" alt="#">
+        <article class="menu__about">
+            <p class="menu__title">Soup</p>
+            <ul class="menu__ingredients">
+                <li>
+                    <p>Ingredient_1</p>
+                    <div class="fill-space"></div>
+                    <p>100 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_2</p>
+                    <div class="fill-space"></div>
+                    <p>200 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_3</p>
+                    <div class="fill-space"></div>
+                    <p>300 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_4</p>
+                    <div class="fill-space"></div>
+                    <p>400 g</p>
+                </li>
+            </ul>
+            <p class="price">8.99$</p>
+        </article>
+    </div>
+    <div class="menu__dish">
+        <img src="assets/imgs/rolls.jpg" alt="#">
+        <article class="menu__about">
+            <p class="menu__title">Rolls</p>
+            <ul class="menu__ingredients">
+                <li>
+                    <p>Ingredient_1</p>
+                    <div class="fill-space"></div>
+                    <p>100 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_2</p>
+                    <div class="fill-space"></div>
+                    <p>200 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_3</p>
+                    <div class="fill-space"></div>
+                    <p>300 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_4</p>
+                    <div class="fill-space"></div>
+                    <p>400 g</p>
+                </li>
+            </ul>
+            <p class="price">4.99$</p>
+        </article>
+    </div>
+    <div class="menu__dish">
+        <img src="assets/imgs/tuna.jpg" alt="#">
+        <article class="menu__about">
+            <p class="menu__title">Tuna</p>
+            <ul class="menu__ingredients">
+                <li>
+                    <p>Ingredient_1</p>
+                    <div class="fill-space"></div>
+                    <p>100 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_2</p>
+                    <div class="fill-space"></div>
+                    <p>200 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_3</p>
+                    <div class="fill-space"></div>
+                    <p>300 g</p>
+                </li>
+                <li>
+                    <p>Ingredient_4</p>
+                    <div class="fill-space"></div>
+                    <p>400 g</p>
+                </li>
+            </ul>
+            <p class="price">19.99$</p>
+        </article>
+    </div>
+</div>
+`
